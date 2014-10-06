24358595ff3655e6b5a0cd8f4f0e8df9cf9ae387
Pawan Kumar <pawan.kumar.13.1991@gmail.com>
2014-10-06 13:15:29 +0530

    removed database texts in settings file

--- guitarchords/settings.py
+++ guitarchords/settings.py
@@ -71,22 +71,22 @@ WSGI_APPLICATION = 'guitarchords.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': { 
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'testdb',                     
-        'USER': 'pakumar',
-        'PASSWORD': 'P@ssw0rd',
-    }
-}      
-
-
 #DATABASES = {
- #   'default': {
-  #      'ENGINE': 'django.db.backends.sqlite3',
-   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #}
-#}
+#    'default': { 
+#        'ENGINE': 'django.db.backends.mysql', 
+#        'NAME': 'testdb',                     
+#        'USER': 'pakumar',
+ #       'PASSWORD': 'P@ssw0rd',
+  #  }
+#}      
+
+
+DATABASES = {
+   'default': {
+      'ENGINE': 'django.db.backends.sqlite3',
+     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+}
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
